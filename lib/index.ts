9101804f58df4445a901d4e0e68ac7a3f8ec9fd8
Admon Sasson <admon.sasson@snyk.io>
2020-10-05 11:42:56 +0300

    fix: support allProjects only in scanAllUnmanaged flag

--- lib/index.ts
+++ lib/index.ts
@@ -117,7 +117,7 @@ export async function inspect(
     targetFile = await createPomForJar(root, targetFile!);
   }
 
-  if (options.scanAllUnmanaged || options.allProjects) {
+  if (options.scanAllUnmanaged) {
     const recursive = !!options.allProjects;
     const jars = findJars(root, recursive);
     if (jars.length > 0) {
