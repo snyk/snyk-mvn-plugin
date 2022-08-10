2105f201883ed29442fd4fb97bf5aed240e1fca4
gitphill <phill.garrett@snyk.io>
2022-08-10 08:16:46 +0100

    refactor: build dep-graph
    Build a Snyk dep-graph from a MavenGraph.
    
    Implement graph pruning. Will be unused today as 'mvn dependency:tree'
    already hides repeat branches, but if we ever want to move to -Dverbose
    and parse larger graphs this will help prune away repeat dependency sub
    graphs.
    
    Test describes the modules behaviour.

---
+++ lib/parse/dep-graph.ts
@@ -0,0 +1,55 @@
+import type { MavenGraph, MavenGraphNode } from './types';
+
+import { DepGraph, DepGraphBuilder, PkgInfo } from '@snyk/dep-graph';
+import { parseDependency } from './dependency';
+
+export function buildDepGraph(mavenGraph: MavenGraph): DepGraph {
+  const { rootId, nodes } = mavenGraph;
+  const root = getPkgInfo(rootId);
+  const builder = new DepGraphBuilder({ name: 'maven' }, root);
+  const visited: string[] = [];
+  const queue: QueueItem[] = [];
+  queue.push(...getItems(rootId, nodes[rootId]));
+
+  // breadth first search
+  while (queue.length > 0) {
+    const item = queue.shift();
+    if (!item) continue;
+    const { id, parentId } = item;
+    const pkgInfo = getPkgInfo(id);
+    if (visited.includes(id)) {
+      const prunedId = id + ':pruned';
+      builder.addPkgNode(pkgInfo, prunedId, { labels: { pruned: 'true' } });
+      builder.connectDep(parentId, prunedId);
+      continue; // don't queue any more children
+    }
+    const parentNodeId = parentId === rootId ? builder.rootNodeId : parentId;
+    builder.addPkgNode(pkgInfo, id);
+    builder.connectDep(parentNodeId, id);
+    queue.push(...getItems(id, nodes[id]));
+    visited.push(id);
+  }
+
+  return builder.build();
+}
+
+interface QueueItem {
+  id: string;
+  parentId: string;
+}
+
+function getItems(parentId: string, node?: MavenGraphNode): QueueItem[] {
+  const items: QueueItem[] = [];
+  for (const id of node?.dependsOn || []) {
+    items.push({ id, parentId });
+  }
+  return items;
+}
+
+function getPkgInfo(value: string): PkgInfo {
+  const { groupId, artifactId, version } = parseDependency(value);
+  return {
+    name: `${groupId}:${artifactId}`,
+    version,
+  };
+}
