e89fd3bb189296c5de479eb16ae1169bc343caae
MarcusArdelean <marcus-emanuel.ardelean@snyk.io>
2024-04-24 15:41:06 +0300

    fix: cyclic deps support for dverbose

---
+++ tests/jest/system/dverbose-flag.spec.ts
@@ -0,0 +1,30 @@
+import * as plugin from "../../../lib";
+import * as path from 'path';
+import { readFixtureJSON } from '../../helpers/read';
+import * as depGraphLib from '@snyk/dep-graph';
+
+
+const testsPath = path.join(__dirname, '../..');
+const fixturesPath = path.join(testsPath, 'fixtures');
+const testProjectPath = path.join(fixturesPath, 'dverbose-project');
+
+test('inspect on dverbose-project pom using -Dverbose', async () => {
+    let result: Record<string, any> = await plugin.inspect(
+      '.',
+      path.join(testProjectPath, 'pom.xml'),
+      {
+        args: ['-Dverbose'],
+      },
+    );
+
+    const expectedJSON = await readFixtureJSON(
+      'dverbose-project',
+      'expected-dverbose-dep-graph.json',
+    );
+    const expectedDepGraph = depGraphLib.createFromJSON(expectedJSON).toJSON();
+    result = result.scannedProjects[0].depGraph?.toJSON();
+
+    expect(result).toEqual(expectedDepGraph);
+  },
+  10000,
+);
